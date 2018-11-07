6156ca74f62db5e7201346a8c8da4ecc8001dc4d
qixiuyuan <972863244@qq.com>
2018-11-07 10:16:25 +0800

    Initial commit

---
+++ src/main/java/io/eblock/eos4j/api/exception/ErrorDetails.java
@@ -0,0 +1,58 @@
+package io.eblock.eos4j.api.exception;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+
+/**
+ * @author mc_q776355102
+ * 
+ * since：2018年10月11日 下午2:26:03
+ */
+public class ErrorDetails {
+
+	private String message;
+
+	private String file;
+
+	private Integer lineNumber;
+
+	private String method;
+
+	private ErrorDetails() {
+
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getFile() {
+		return file;
+	}
+
+	public void setFile(String file) {
+		this.file = file;
+	}
+
+	public Integer getLineNumber() {
+		return lineNumber;
+	}
+
+	@JsonProperty("line_number")
+	public void setLineNumber(Integer lineNumber) {
+		this.lineNumber = lineNumber;
+	}
+
+	public String getMethod() {
+		return method;
+	}
+
+	public void setMethod(String method) {
+		this.method = method;
+	}
+
+}
